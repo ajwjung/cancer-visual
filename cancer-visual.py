aae94aa27209372e04d74c9ee07b43c5c2470a0d
Amy <ajjung@ucsc.edu>
2022-03-28 03:35:14 -0700

    Add ability to graph using python

--- cancer-visual.py
+++ cancer-visual.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import matplotlib.pyplot as plt
 
 # Import data
 df = pd.read_csv("idh1-brain-mutations.tsv", sep="\t", header=0)
@@ -10,3 +11,14 @@ consequences = consequences.where(consequences.notnull(), None) # Replace "NaN" 
 # Obtain frequencies for each consequence type
 cons_freq = consequences.stack().value_counts() # series
 freq = cons_freq.rename_axis("Consequences").reset_index(name="Frequency") # dataframe
+
+# Create a bar plot
+fig, ax = plt.subplots()
+ax.bar(freq["Consequences"], freq["Frequency"])
+ax.set_xlabel("Consequence Type")
+ax.set_ylabel("# of Mutations")
+ax.set_title("Frequencies of IDH1 Mutation Consequences")
+ax.tick_params(axis='both', which='major', labelsize=8) # resize labels
+plt.setp(ax.get_xticklabels(), rotation=30, horizontalalignment='right') # rotate x-labels
+
+plt.show()
